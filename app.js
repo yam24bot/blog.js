496fd96e717c1126b917887e54c3493bfb6cbf73
yam24bot <danil180401@gmail.com>
2018-12-29 02:34:03 +0200

    #04_create_layout

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-const Post = require("./models/post.js");
+const path = require("path");
 
 var app = express();
 
 app.set("view engine", "ejs"); // Установка шаблонизатора ejs
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //Отдача css, js, images
+
+app.use(
+  "/javascripts",
+  express.static(path.join(__dirname, "node_modules", "jquery", "dist"))
+); //Добавление jquery
 
 app.get("/", function(req, res) {
-  Post.find({}).then(posts => {
-    res.render("index", { posts: posts });
-  });
-});
-
-app.get("/create", function(req, res) {
-  res.render("create");
-});
-
-app.post("/create", function(req, res) {
-  const { title, body } = req.body; //Рестукуризация
-
-  Post.create({
-    title: title,
-    body: body
-  }).then(post => console.log(post.id));
-  res.redirect("/");
+  res.render("index");
 });
 
 module.exports = app;
